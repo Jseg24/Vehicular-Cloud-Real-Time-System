c299271023b5b6552e5d02ee6797bfefe5ee460e
“Karl <karl.reid21@my.stjohns.edu>
2025-02-23 14:55:45 -0500

    UI Update

--- src/package1/UserButtons.java
+++ src/package1/UserButtons.java
@@ -2,43 +2,76 @@ package package1;
 
 import java.awt.Color;
 import java.awt.Dimension;
-
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 public class UserButtons extends JPanel {
-	
-private JButton user;
-private JButton client;
-Border emptyborder = BorderFactory.createEmptyBorder();
 
-public UserButtons() {
-	this.setPreferredSize(new Dimension (400,30));
-	this.setBackground(Color.black);
-	
-	user = new JButton("USER");
-	user.setBorder(emptyborder);
-	user.setToolTipText("Click if User");
-	user.setPreferredSize(new Dimension (150,50));
+	private JButton user;
+	private JButton client;
 
-	
-	this.add(user);
-	this.add(Box.createHorizontalStrut(60));
-	client = new JButton("CLIENT");
-	client.setBorder(emptyborder);
-	client.setToolTipText("Click if client");
-	client.setPreferredSize(new Dimension (150,50));
-	this.add(client);
-}
-public JButton getUserBtn() {
-	return user;
-	
-}
-public JButton getClientBtn() {
-	return client;
-}
+	// Static block to set the Nimbus Look and Feel
+	Border emptyborder = BorderFactory.createEmptyBorder();
+
+	static {
+		try {
+
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException
+				| IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public UserButtons() {
+		this.setPreferredSize(new Dimension(400, 30));
+		this.setBackground(Color.black);
+
+		user = new JButton("USER");
+
+		user.setToolTipText("Click if User");
+		user.setPreferredSize(new Dimension(150, 50));
+		this.add(user);
+		user.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		
+		user.setBorder(new CompoundBorder(new LineBorder(Color.WHITE, 2), new EmptyBorder(5, 10, 5, 10)));
+
+		this.add(Box.createHorizontalStrut(60));
+		
+		client = new JButton("CLIENT");
+
+		client.setToolTipText("Click if client");
+		client.setPreferredSize(new Dimension(150, 50));
+		this.add(client);
+
+		client.setFont(new Font("Times New Roman", Font.BOLD, 14));
+
+		// Add border
+		client.setBorder(new CompoundBorder(new LineBorder(Color.WHITE, 2), new EmptyBorder(5, 10, 5, 10)));
+
+	}
+
+	public JButton getUserBtn() {
+		return user;
+
+	}
+
+	public JButton getClientBtn() {
+		return client;
+	}
 }
