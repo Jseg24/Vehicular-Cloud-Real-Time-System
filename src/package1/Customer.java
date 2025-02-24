e8e2afe1949149626139c9861f62f41998c3f102
dinarism <145953407+dinarism@users.noreply.github.com>
2025-02-24 15:27:39 -0500

    Update Customer.java

--- src/package1/Customer.java
+++ src/package1/Customer.java
@@ -1,6 +1,5 @@
 package package1;
 
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -34,8 +33,7 @@ public class Customer extends JFrame implements ActionListener {
 	private JLabel deadline;
 	private JPanel panel;
 	private JPanel panelButton;
-	
-	
+
 	public Customer() {
 
 		createButton();
@@ -44,10 +42,10 @@ public class Customer extends JFrame implements ActionListener {
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Client Page");
-		this.setLayout(new BorderLayout());		
+		this.setLayout(new BorderLayout());
 		this.setSize(500, 500);
 		this.setResizable(false);
-		panel.setBorder(new EmptyBorder(150,75,150,75));
+		panel.setBorder(new EmptyBorder(150, 75, 150, 75));
 		this.add(panel, BorderLayout.CENTER);
 		this.add(panelButton, BorderLayout.SOUTH);
 
@@ -55,7 +53,9 @@ public class Customer extends JFrame implements ActionListener {
 
 	}
 
-	
+	public static void main(String[] args) {
+		new Customer();
+	}
 
 	public void createButton() {
 		submit = new JButton();
@@ -65,7 +65,7 @@ public class Customer extends JFrame implements ActionListener {
 
 		back = new JButton();
 		back.addActionListener(this);
-		back.setText("back");
+		back.setText("Back");
 		back.setFocusable(false);
 
 		panelButton = new JPanel();
@@ -80,33 +80,29 @@ public class Customer extends JFrame implements ActionListener {
 		Client = new JLabel();
 		Client.setText("Client ID (6 digits): ");
 		Client.setHorizontalAlignment(SwingConstants.CENTER);
-	    Client.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		Client.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 		client_ID = new JTextField();
-		client_ID.setPreferredSize(new Dimension(10,10));
+		client_ID.setPreferredSize(new Dimension(10, 10));
 
 		duration = new JLabel();
-		duration.setText("Approximate Job Duration (hours): ");
+		duration.setText("Approximate Job Duration (In Hours): ");
 		duration.setHorizontalAlignment(SwingConstants.CENTER);
 		duration.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 		jobDuration = new JTextField();
-		jobDuration.setPreferredSize(new Dimension(10,10));
-
+		jobDuration.setPreferredSize(new Dimension(10, 10));
 
 		deadline = new JLabel();
 		deadline.setText("Job Deadline (Enter MM/DD/YYYY): ");
 		deadline.setHorizontalAlignment(SwingConstants.CENTER);
 		deadline.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 		jobDeadline = new JTextField();
-		jobDeadline.setPreferredSize(new Dimension(10,10));
-		
-		
+		jobDeadline.setPreferredSize(new Dimension(10, 10));
 
 	}
 
 	public void createPanel() {
 		panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		
 
 		JPanel panel1 = new JPanel();
 		panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
@@ -125,52 +121,48 @@ public class Customer extends JFrame implements ActionListener {
 		deadline.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 		panel3.add(deadline);
 		panel3.add(jobDeadline);
-		
-		
+
 		panel.add(panel1);
 		panel.add(panel2);
 		panel.add(panel3);
-		
 
 		add(panel);
 	}
-	 // Method to Validate Date Format (MM-DD-YYYY)
-    private boolean isValidDate(String jobDeadline) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-        dateFormat.setLenient(false); // Ensure strict format checking
-        try {
-            dateFormat.parse(jobDeadline);
-            return true;
-        } catch (ParseException e) {
-            return false;
-        }
-    }
-    
+
+	// Method to Validate Date Format (MM-DD-YYYY)
+	private boolean isValidDate(String jobDeadline) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		dateFormat.setLenient(false); // Ensure strict format checking
+		try {
+			dateFormat.parse(jobDeadline);
+			return true;
+		} catch (ParseException e) {
+			return false;
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == submit) {
-            try {
-                // Convert inputs from text fields to ints
-                int clientID = Integer.parseInt(client_ID.getText());
-                int jobHours = Integer.parseInt(jobDuration.getText().trim());
-                
-                String deadlineText = jobDeadline.getText().trim();
-                if (!isValidDate(deadlineText)) {
-                    System.out.println("Error: Invalid date format. Please use MM-DD-YYYY.");
-                    return;
-                }
-               
-                new Storing(clientID, jobHours, deadlineText);
-                this.dispose();
-                new UserInfoReceiver();
-            } 
-            catch (NumberFormatException ex) {
-                System.out.println("Error: Invalid input! Ensure ID, Year, and Residency Time are numbers.");
-            }
-            }
-    
-			
-		
+			try {
+				// Convert inputs from text fields to ints
+				int clientID = Integer.parseInt(client_ID.getText());
+				int jobHours = Integer.parseInt(jobDuration.getText().trim());
+
+				String deadlineText = jobDeadline.getText().trim();
+				if (!isValidDate(deadlineText)) {
+					System.out.println("Error: Invalid date format. Please use MM-DD-YYYY.");
+					return;
+				}
+
+				new Storing(clientID, jobHours, deadlineText);
+				this.dispose();
+				new UserInfoReceiver();
+			} catch (NumberFormatException ex) {
+				System.out.println("Error: Invalid input! Ensure ID, Year, and Residency Time are numbers.");
+			}
+		}
+
 		if (e.getSource() == back) {
 			this.dispose();
 			new UserInfoReceiver();
@@ -178,7 +170,3 @@ public class Customer extends JFrame implements ActionListener {
 
 	}
 }
-            
-
-		
-
